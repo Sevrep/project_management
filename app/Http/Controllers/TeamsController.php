ec779199b655deac60979d4e6a52e5871d340272
Sevrep <62856034+Sevrep@users.noreply.github.com>
2021-05-14 18:28:55 +0800

    add teams model and controller

---
+++ app/Http/Controllers/TeamsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Teams;
+
+class TeamsController extends Controller
+{
+    public function index()
+    {
+        return Teams::orderBy('created_at', 'DESC')->get();
+    }
+
+    public function store(Request $request)
+    {
+        $newTeams = new Teams;
+        $newTeams->teams_name = $request->teams["teams_name"];
+        $newTeams->teams_created_by = $request->teams["teams_created_by"];
+        $newTeams->save();
+        return $newTeams;
+    }
+
+    public function update(Request $request, $teams_id)
+    {
+        $existingTeams = Teams::find($teams_id);
+
+        if ($existingTeams) {
+            $existingTeams->teams_name = $request->teams['teams_name'];
+            $existingTeams->teams_created_by = $request->teams['teams_created_by'];
+            $existingTeams->save();
+            return $existingTeams;
+        }
+        return "Teams not found.";
+    }
+
+    public function destroy($teams_id)
+    {
+        $existingTeams = Teams::find($teams_id);
+
+        if ($existingTeams) {
+            $existingTeams->delete();
+            return "Teams " . $teams_id . " successfully deleted.";
+        }
+        return "Teams not found.";
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeamsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/teams', [TeamsController::class, 'index']);
+Route::prefix('/team')->group(function() {
+    Route::post('/store', [TeamsController::class, 'store']);
+    Route::put('/{teams_id}', [TeamsController::class, 'update']);
+    Route::delete('/{teams_id}', [TeamsController::class, 'destroy']);
 });
